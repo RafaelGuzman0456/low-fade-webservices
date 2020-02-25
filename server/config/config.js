3aed63b983019470b86b35f7a6d911e54f048058
Rafael <guzmanrafael0456@gmail.com>
2020-02-25 12:51:46 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,6 +21,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/low-fade';
 } else {
-    urlDB = 'mongodb+srv://rafael:rafael123@low-fade-pkoom.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
